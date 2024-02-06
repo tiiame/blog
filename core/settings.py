abbf05b3cedc7830350d622342d97d968d45be72
Suhrob <atavullayevsuhrob@gmail.com.com>
2024-02-06 21:56:11 +0500

    suhrob

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2l7qtb&r*&d138*u&+c&a+869n4#bblgknvxw%$sjv&cp8g)bs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
