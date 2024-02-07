5ceb622b8d1c7b9124d603d96b6541f8053587c3
Suhrob <atavullayevsuhrob@gmail.com.com>
2024-02-07 20:19:54 +0500

    changes

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2l7qtb&r*&d138*u&+c&a+869n4#bblgknvxw%$sjv&cp8g)bs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
